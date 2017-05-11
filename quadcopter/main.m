4cc66efdd6227f8fba70fc255563799f56f68773
valstrl <adrian@tsf-460-wpa-3-108.epfl.ch>
2017-05-11 14:59:18 +0200

    Initial Upload

---
+++ quadcopter/main.m
@@ -0,0 +1,49 @@
+% loads:
+%    hovering equilibrium (xs,us)
+%    continuous time matrices Ac,Bc of the linearization
+%    matrices sys.A, sys.B of the inner-loop discretized with sampling period sys.Ts
+%    outer controller optimizer instance
+load('quadData.mat')
+outerController = getOuterController(Ac, 'cplex');
+disp('Data successfully loaded')
+
+
+
+%% %%%%%%%%%%%%%% First MPC controller %%%%%%%%%%%%%%%%%%%
+
+
+pause
+
+%% Reference tracking - no disturbance, no invariant sets
+fprintf('PART II - reference tracking...\n')
+
+pause
+
+%% Nonlinear model simulation - no disturbance
+fprintf('Running the FIRST NL model simulation...\n')
+%sim('simulation1.mdl') 
+
+pause
+
+%% Disturbance estimation
+%estimator
+
+
+%% Offset free MPC
+fprintf('PART III - OFFSET FREE / Disturbance rejection...\n')
+
+pause
+%% Final simulation
+fprintf('Running the FINAL NL model simulation...\n')
+%sim('simulation2.mdl') 
+pause
+%% BONUS - Slew rate constraints
+% run after doing nonlinear simulations otherwise the NL simulations won't
+% work (because of the additional controller argument)
+fprintf('BONUS - SLEW RATE CONSTRAINTS...\n')
+
+
+
+
+
+
